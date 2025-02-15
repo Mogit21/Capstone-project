77ceece7322f0d17a551621267d36e4ed9a1989a
Mogit21 <mohamedhaziane@gmail.com>
2025-02-15 01:19:39 -0400

    commit before sbmit

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -24,6 +24,7 @@ router.register(r'booking', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('restaurant.urls')),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/', include(router.urls)),
     path('auth/', include('djoser.urls')),
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,4 @@
-#define URL route for index() view
+
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
